9d9466eb7557ae7e8cbe9a4e267ab262b581d93d
Travis Bauer <travis.bauer@smalldataproblem.org>
2025-09-25 22:06:03 -0600

    Removed Signatures segments

--- src/talkpipe/__init__.py
+++ src/talkpipe/__init__.py
@@ -16,7 +16,6 @@ from talkpipe.data.mongo import *
 from talkpipe.operations.filtering import *
 from talkpipe.operations.transforms import *
 from talkpipe.operations.matrices import *
-from talkpipe.operations.signatures import *
 from talkpipe.app.chatterlang_serve import *
 from talkpipe.search.whoosh import *
 from talkpipe.search.simplevectordb import *
